4ca99b163d4da5f623cfef2379c29affbf7d359d
naraisneus <105355670+naraisneus@users.noreply.github.com>
2024-04-15 11:13:22 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from machine import Pin
+
 def on_forever():
-    pass
+    LDR=3
+    #LDR=pins.A0
+    print("LDR: "+ LDR)
+    if 2<LDR<=5:
+        serial.write_value("x", LDR)
+        #pins.D0 = LDR
+    else:
+        print('res')
+
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 forever(function on_forever() {
+    let LDR = 3
+    // LDR=pins.A0
+    console.log("LDR: " + LDR)
+    if (2 < LDR && LDR <= 5) {
+        serial.writeValue("x", LDR)
+    } else {
+        // pins.D0 = LDR
+        console.log("res")
+    }
     
 })
